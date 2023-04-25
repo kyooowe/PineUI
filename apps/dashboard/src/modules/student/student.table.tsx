c7a75f81c2e8b5e9bd4d1854409803ad91b60aba
kyooowe <brinasjohnmark@gmail.com>
2023-04-25 23:53:36 +0800

    Changes
    UI
    - Fix issue showing the empty list instead of loader

--- apps/dashboard/src/modules/student/student.table.tsx
+++ apps/dashboard/src/modules/student/student.table.tsx
@@ -535,25 +535,25 @@ const StudentTable = memo(({ studentCount, setStudentCount }: IStudentTableProps
                 </div>
             </div>
             {
-                isListEmpty ? (
-                    <div className="flex items-center mt-6 text-center border rounded-lg h-96 dark:border-gray-700">
-                        <div className="flex flex-col w-full max-w-sm px-4 mx-auto">
-                            <div className="p-3 mx-auto text-blue-500 bg-blue-100 rounded-full dark:bg-gray-800">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                                </svg>
-                            </div>
-                            <h1 className="mt-3 text-lg text-gray-800 dark:text-white">No students found</h1>
-                            <p className="mt-2 text-gray-500 dark:text-gray-400">We're sorry, there are no records to display at this time. Please check back later or contact customer support for assistance. Thank you for your understanding.</p>
-                        </div>
-                    </div>
+                isFetching ? (
+                    <CenterLoader />
                 ) : (
-                    <div className='flex flex-col mt-2'>
+                    <>
                         {
-                            isFetching ? (
-                                <CenterLoader height='h-72' />
+                            isListEmpty ? (
+                                <div className="flex items-center mt-6 text-center border rounded-lg h-96 dark:border-gray-700">
+                                    <div className="flex flex-col w-full max-w-sm px-4 mx-auto">
+                                        <div className="p-3 mx-auto text-blue-500 bg-blue-100 rounded-full dark:bg-gray-800">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                                            </svg>
+                                        </div>
+                                        <h1 className="mt-3 text-lg text-gray-800 dark:text-white">No students found</h1>
+                                        <p className="mt-2 text-gray-500 dark:text-gray-400">We're sorry, there are no records to display at this time. Please check back later or contact customer support for assistance. Thank you for your understanding.</p>
+                                    </div>
+                                </div>
                             ) : (
-                                <>
+                                <div className='flex flex-col mt-2'>
                                     <div className='overflow-x-auto'>
                                         <div className='inline-block min-w-full py-2 align-middle'>
                                             <div className='overflow-hidden border border-gray-300 rounded-lg dark:border-gray-700'>
@@ -583,10 +583,10 @@ const StudentTable = memo(({ studentCount, setStudentCount }: IStudentTableProps
                                     </div>
 
                                     <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePaginationChange} />
-                                </>
+                                </div>
                             )
                         }
-                    </div>
+                    </>
                 )
             }
 
