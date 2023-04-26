826e67a970d2ecee28d9793de8a3505f43b25189
kyooowe <brinasjohnmark@gmail.com>
2023-04-27 00:13:18 +0800

    Changes
    UI
    - Change the header class of card from py-5 to py-3
    - Make the icon of studentCardHeader white when darkmode

--- apps/dashboard/src/components/cards.component.tsx
+++ apps/dashboard/src/components/cards.component.tsx
@@ -4,7 +4,7 @@ import { ICard } from "../interface/components/card.interface";
 const Card = memo(({ header, body, isPlain = false }: ICard) => {
     return (
         <div className="w-auto h-full bg-white shadow-lg rounded-lg overflow-hidden border border-gray-300 dark:bg-gray-700 dark:border-gray-500">
-            <div className={`px-5 py-5 ${isPlain ? '' : ' border-b dark:border-gray-500'}`}>
+            <div className={`px-5 py-3 ${isPlain ? '' : ' border-b dark:border-gray-500'}`}>
                 {header}
             </div>
             <div className="px-5 py-2">
