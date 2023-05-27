973295cb29649a0d72bc67577f78e9a8e5fb2e35
kyooowe <brinasjohnmark@gmail.com>
2023-05-27 18:45:44 +0800

    Changes
    UI
    - Fix issue in deployment in google-button

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
-        rollupOptions: {
-            external: ["tailwind-merge"],
-        },
     },
 })
