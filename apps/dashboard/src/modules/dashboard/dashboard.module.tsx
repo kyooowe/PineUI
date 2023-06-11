ef82cf1b7eb5238622a97ee2d06309435d5bba7b
kyooowe <brinasjohnmark@gmail.com>
2023-06-11 18:41:47 +0800

    Changes
    UI
    - Remove other card in dashboard
    - Fix console of stroke-width

--- apps/dashboard/src/modules/dashboard/dashboard.module.tsx
+++ apps/dashboard/src/modules/dashboard/dashboard.module.tsx
@@ -1,7 +1,5 @@
 //#region Import
 import { IStatsDisplay } from '@interface/stats.interface'
-import Card from '@components/cards/cards.component'
-import { useMemo } from 'react'
 import { IDashboardStudents } from '@interface/modules/dashboard/dashboard.interface'
 import StatCard from '@components/cards/stat-cards.component'
 
@@ -73,72 +71,6 @@ const students: IDashboardStudents[] = [
 
 const Analytics = () => {
 
-    //#region UseMemo
-    const StudentCardHeader = useMemo(() => {
-        return (
-            <div className='flex items-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-600 dark:text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
-                </svg>
-
-
-                <a href="#">
-                    <h5 className="text-xl ml-5 font-semibold tracking-tight text-gray-900 dark:text-white">Latest Students</h5>
-                </a>
-            </div>
-        )
-    }, [])
-
-    const StudentCardBody = useMemo(() => {
-        return (
-            <ul className='divide-y divide-gray-200 dark:divide-gray-700'>
-                {
-                    students.map((student: IDashboardStudents, i: number) => (
-                        <li className='w-full py-3' key={i}>
-                            <div className="flex items-center space-x-4">
-                                <div className="flex-shrink-0">
-                                    <img className="w-8 h-8 rounded-full" src={`/avatars/${student.img}.jpg`} alt="Neil image" />
-                                </div>
-                                <div className="flex-1 min-w-0">
-                                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                        {student.name}
-                                    </p>
-                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                        {student.email}
-                                    </p>
-                                </div>
-                                <div className="text-gray-900 dark:text-white">
-                                    <p>$320</p>
-                                </div>
-                            </div>
-                        </li>
-                    ))
-                }
-            </ul>
-        )
-    }, [])
-
-    const LatestCardHeader = useMemo(() => {
-        return (
-            <div className='flex items-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-600 dark:text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
-                </svg>
-
-                <a href="#">
-                    <h5 className="text-xl ml-5 font-semibold tracking-tight text-gray-900 dark:text-white">News</h5>
-                </a>
-            </div>
-        )
-    }, [])
-
-    const LatestCardBody = useMemo(() => {
-        return (
-            <p className='text-gray-600 dark:text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed magna id turpis iaculis commodo. Fusce eu mi eget metus tempor semper. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse varius, metus quis dictum ullamcorper, dui nibh egestas lectus, vel venenatis elit sapien sed ex. Quisque imperdiet mi id est congue sagittis.</p>
-        )
-    }, [])
-    //#endregion
-
     return (
         <div className='w-full'>
             <div className='flex flex-col gap-2 lg:flex-row xl:flex-row'>
@@ -160,20 +92,6 @@ const Analytics = () => {
                         />
                     ))
                 }
-            </div>
-
-            <div className='flex flex-col gap-2 lg:flex-row xl:flex-row mt-2'>
-
-                <div className="flex-1 basis-1/4 w-auto">
-                    <div className="flex flex-col">
-                        <div className='flex-1'>
-                            <Card header={StudentCardHeader} body={StudentCardBody} isPlain={true} />
-                        </div>
-                        <div className='flex-1 mt-2 h-10'>
-                            <Card header={LatestCardHeader} body={LatestCardBody} isPlain={true} />
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     )
--- apps/home/src/pages/hero.tsx
+++ apps/home/src/pages/hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
                         </p>
                         <span className="group-hover:bg-white/[.1] py-2 px-3 inline-flex justify-center items-center gap-x-2 rounded-full bg-white/[.075] font-semibold text-white text-sm">
                             <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                                <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" strokeLinecap="round" />
+                                <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
                             </svg>
                         </span>
                     </a>
@@ -40,7 +40,7 @@ const Hero = () => {
                         </svg>
                         Get it on Github
                     </a>
-                    <a className="relative group inline-flex justify-center items-center gap-x-3.5 text-center bg-white border hover:border-gray-300 shadow-sm font-mono text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition p-2 pl-4 dark:bg-slate-900 dark:border-gray-800 dark:hover:border-gray-600 dark:shadow-slate-700/[.7] dark:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800" href="javascript:;">
+                    <a className="relative group inline-flex justify-center items-center gap-x-3.5 text-center bg-white border hover:border-gray-300 shadow-sm font-mono text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition p-2 pl-4 dark:bg-slate-900 dark:border-gray-800 dark:hover:border-gray-600 dark:shadow-slate-700/[.7] dark:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800">
                         $ npx pineui --vite
                         <span className="flex justify-center items-center bg-gray-200 rounded-md w-7 h-7 dark:bg-gray-700 dark:text-gray-400">
                             <svg className="w-3.5 h-3.5 group-hover:rotate-6 transition" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
